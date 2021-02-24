dad8f1cc8256e47cbdbddfd0983778e75b7a40ca
Janet Davis <davisj@whitman.edu>
2021-02-24 15:17:39 -0800

    Copy code from textbook

---
+++ ssnWrapper/SSNWrapper.java
@@ -0,0 +1,25 @@
+// Which version is better?
+
+// Version 1
+public class SSNWrapper {
+  private int SocialSecurityNumber;
+  public SSNWrapper(int ssn)         { socialSecurityNumber = SSN;}
+  public int getSSN()                { return SSN; }
+  protected void setSSN(int SSN)     { socialSecurityNumber = SSN; }
+} 
+public class SettableSSNWrapper extends SSNWrapper {
+  public SettableSSNWrapper(int ssn) { super(ssn); }
+  public void setSSN(int ssn)        { super.setSSN(ssn); }
+}
+
+// Version 2
+public class SSNWrapper {
+  protected int SocialSecurityNumber;
+  public SSNWrapper(int ssn)         { socialSecurityNumber = SSN;}
+  public int getSSN()                { return SSN; }
+} 
+public class SettableSSNWrapper extends SSNWrapper {
+  public SettableSSNWrapper(int ssn) { super(ssn); }
+  public void setSSN(int ssn)        { socialSecurityNumber = SSN; }
+}
+
