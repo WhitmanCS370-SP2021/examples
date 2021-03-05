9a2829cd11c57cc2abbed9088da7f621aa47f44d
Janet Davis <davisj@whitman.edu>
2021-03-05 10:46:56 -0800

    Make code run

--- chapter3/Sorter.java
+++ chapter3/Sorter.java
@@ -1,16 +1,22 @@
-public interface Comparator {
-  public static int compare(Object x, Object y);
+interface Comparator {
+  public int compare(Object x, Object y);
 }
 
-public class IntegerComparator implements Comparator {
-    public static int compare(Object x, Object y) {
+class IntegerComparator implements Comparator {
+    public int compare(Object x, Object y) {
       return (Integer) x - (Integer) y;
     }
 }
 
-public class StringComparator implements Comparator {
-    public static int compare(Object x, Object y) {
+class StringComparator implements Comparator {
+    public int compare(Object x, Object y) {
        return ((String)x).compareTo((String)y);
+    }
+}
+
+class StringIgnoreCaseComparator implements Comparator {
+    public int compare(Object x, Object y) {
+       return ((String)x).compareToIgnoreCase((String)y);
     }
 }
 
@@ -21,12 +27,22 @@ public class Sorter {
       // swap the largets value in data[0]..dta[i] into position i
       int indexOfMax = 0;
       for (int j=1; j <= i; j++) {
-        if (comp.compare(data[j], data[indexOfMax]) > 0)
-          indexOfMax = j
+        if (comp.compare(data[j], data[indexOfMax]) > 0) {
+          indexOfMax = j;
+        }
       }
       Object temp = data[i];
       data[i] = data[indexOfMax];
-      data[indexOfMax] = tmp;
+      data[indexOfMax] = temp;
     }
+  }
+  public static void main(String[] args) {
+     String[] strings = {"Leila", "Laska", "Vreni", "Ryan", "Stephanie", "Tony", "Haley"};
+     sort(strings, new StringComparator());
+     for (String s : strings) System.out.println(s);
+
+     Integer[] integers = {42, 17, 0, -1, 1, 6, 8, 255};
+     sort(integers, new IntegerComparator());
+     for (int i : integers) System.out.println(i);
   }
 }
