2f418818aaa077804badc2c1c1d9d0510b52db07
Janet Davis <davisj@whitman.edu>
2021-03-26 14:19:21 -0700

    initial version

---
+++ cardealer/CarDealer.java
@@ -0,0 +1,14 @@
+class CarDealer {
+    Collection<Car> cars;
+
+    Collection<Car> filterByPrice(range)
+
+    Collection<Car> filterByYear(yr)
+
+    filterByMake(make)
+
+    filterByModel(model)
+
+    filterByColor(color)
+
+}
