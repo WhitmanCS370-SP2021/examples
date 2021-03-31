c04d81a196f04a51147f3b99a1eb0a8962f91342
Janet Davis <davisj@whitman.edu>
2021-03-31 11:17:51 -0700

    Replace tabs with spaces

--- quiz4/Shape.java
+++ quiz4/Shape.java
@@ -1,38 +1,38 @@
 public class Shape {
-	public static final int LINE = 1;
-	public static final int CIRCLE = 2;
-	public static final int TRIANGLE = 3;
-	public static final int RECTANGLE = 4;
+    public static final int LINE = 1;
+    public static final int CIRCLE = 2;
+    public static final int TRIANGLE = 3;
+    public static final int RECTANGLE = 4;
 
-	private int shapeType;
-	private double width, height;
+    private int shapeType;
+    private double width, height;
 
-	public Shape(int shapeType, double width, double height) {
+    public Shape(int shapeType, double width, double height) {
         this.shapeType = shapeType; 
         this.width = width;
-		this.height = height;
-	}
+        this.height = height;
+    }
 
-	public int getShapeType() { 
-		return shapeType; 
-	}
+    public int getShapeType() { 
+        return shapeType; 
+    }
 
-	public double area() {
-		if (shapeType == LINE) {
-        	throw new UnsupportedOperationException();
+    public double area() {
+        if (shapeType == LINE) {
+            throw new UnsupportedOperationException();
         } 
         else if (shapeType == CIRCLE) {
-        	return 3.14159 * width * width / 4.0;
+            return 3.14159 * width * width / 4.0;
         }
         else if (shapeType == TRIANGLE) {
-        	return width * height / 2.0;
+            return width * height / 2.0;
         }
         else if (shapeType == RECTANGLE) {
-        	return width * height;
+            return width * height;
         }
         else {
-        	return 0;
+            return 0;
         }
-	}
+    }
 }
 
